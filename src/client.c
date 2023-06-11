c945c7a8f5d70dee735bd61b66f8b1aa4c440689
Vivallo04 <andresvivallo4@gmail.com>
2023-06-10 23:57:19 -0600

    Refactor file

--- src/client.c
+++ src/client.c
@@ -8,41 +8,41 @@ WSADATA wsaData;
 struct sockaddr_in serverAddress;
 
 // Create a socket for connecting to the server
-SOCKET clientSocket = INVALID_SOCKET;
+SOCKET client_socket = INVALID_SOCKET;
 
-int configure_client_socket(struct WSAData wsaData, struct sockaddr_in serverAddress, SOCKET clientSocket) {
-    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+int configure_client_socket(struct WSAData data, struct sockaddr_in server_address) {
+    if (WSAStartup(MAKEWORD(2, 2), &data) != 0) {
         printf("Failed to initialize Winsock.\n");
         return 1;
     }
 
-    if (clientSocket == INVALID_SOCKET) {
+    if (socket == INVALID_SOCKET) {
         printf("Failed to create socket.\n");
         WSACleanup();
         return 1;
     }
 
-    clientSocket = socket(AF_INET, SOCK_STREAM, 0);
+    client_socket = socket(AF_INET, SOCK_STREAM, 0);
 
-    serverAddress.sin_family = AF_INET;
-    serverAddress.sin_port = htons(1234); // Set the server port number
-    serverAddress.sin_addr.s_addr = inet_addr("127.0.0.1"); // Set the server IP address
+    server_address.sin_family = AF_INET;
+    server_address.sin_port = htons(1234); // Set the server port number
+    server_address.sin_addr.s_addr = inet_addr("127.0.0.1"); // Set the server IP address
 
     printf("Successfully initialized client.\n");
 
     // Connect to the server
-    if (connect(clientSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress)) < 0) {
+    if (connect(client_socket, (struct sockaddr*)&server_address, sizeof(server_address)) < 0) {
         printf("Failed to connect to the server.\n");
-        closesocket(clientSocket);
+        closesocket(client_socket);
         WSACleanup();
         return 1;
     }
 }
 
-int send_message_to_server(SOCKET clientSocket, const char* message) {
-    if (send(clientSocket, message, strlen(message), 0) < 0) {
+int send_message_to_server(SOCKET client_socket, const char* message) {
+    if (send(client_socket, message, strlen(message), 0) < 0) {
         printf("Failed to send data to the server.\n");
-        closesocket(clientSocket);
+        closesocket(client_socket);
         WSACleanup();
         return 1;
     }
@@ -50,13 +50,13 @@ int send_message_to_server(SOCKET clientSocket, const char* message) {
     printf("Successfully sent message to server.\n");
 }
 
-int get_server_response(SOCKET client_socket, char serverResponse[1024]) {
-    if (recv(client_socket, serverResponse, sizeof(serverResponse), 0) < 0) {
+int get_server_response(SOCKET client_socket, char server_response[1024]) {
+    if (recv(client_socket, server_response, sizeof(server_response), 0) < 0) {
         printf("Failed to receive data from the server.\n");
         closesocket(client_socket);
         WSACleanup();
         return 1;
     }
 
-    printf("Server response: %s\n", serverResponse);
+    printf("Server response: %s\n", server_response);
 }
