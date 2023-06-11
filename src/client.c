e0639472ee598543a215051123b51c75c87f8f7f
Vivallo04 <andresvivallo4@gmail.com>
2023-06-11 00:57:29 -0600

    Client refactor

--- src/client.c
+++ src/client.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 #include <winsock2.h>
 
+#pragma comment(lib, "ws2_32.lib")
 
-int configure_client_socket(struct WSAData data, struct sockaddr_in server_address, SOCKET client_socket) {
+int configure_client_socket(struct WSAData data, struct sockaddr_in server_address, SOCKET* client_socket) {
     if (WSAStartup(MAKEWORD(2, 2), &data) != 0) {
         printf("Failed to initialize Winsock.\n");
         return 1;
     }
 
-    if (socket == INVALID_SOCKET) {
+    *client_socket = socket(AF_INET, SOCK_STREAM, 0);
+    if (*client_socket == INVALID_SOCKET) {
         printf("Failed to create socket.\n");
         WSACleanup();
         return 1;
     }
-
-    client_socket = socket(AF_INET, SOCK_STREAM, 0);
 
     server_address.sin_family = AF_INET;
     server_address.sin_port = htons(1234); // Set the server port number
@@ -23,32 +23,41 @@ int configure_client_socket(struct WSAData data, struct sockaddr_in server_addre
     printf("Successfully initialized client.\n");
 
     // Connect to the server
-    if (connect(client_socket, (struct sockaddr*)&server_address, sizeof(server_address)) < 0) {
+    if (connect(*client_socket, (struct sockaddr*)&server_address, sizeof(server_address)) == SOCKET_ERROR) {
         printf("Failed to connect to the server.\n");
-        closesocket(client_socket);
+        closesocket(*client_socket);
         WSACleanup();
         return 1;
     }
+
+    return 0;
 }
 
 int send_message_to_server(SOCKET client_socket, const char* message) {
-    if (send(client_socket, message, strlen(message), 0) < 0) {
+    int result = send(client_socket, message, strlen(message), 0);
+    if (result == SOCKET_ERROR) {
         printf("Failed to send data to the server.\n");
-        closesocket(client_socket);
-        WSACleanup();
         return 1;
     }
 
     printf("Successfully sent message to server.\n");
+    return 0;
 }
 
 int get_server_response(SOCKET client_socket, char server_response[1024]) {
-    if (recv(client_socket, server_response, sizeof(server_response), 0) < 0) {
+    int result = recv(client_socket, server_response, 1024, 0);
+    if (result == SOCKET_ERROR) {
         printf("Failed to receive data from the server.\n");
-        closesocket(client_socket);
-        WSACleanup();
         return 1;
     }
 
+    server_response[result] = '\0'; // Null-terminate the received data
     printf("Server response: %s\n", server_response);
+    return 0;
+}
+
+int close_socket(SOCKET client_socket) {
+    closesocket(client_socket);
+    WSACleanup();
+    return 0;
 }
--- src/main.c
+++ src/main.c
@@ -1,60 +1,41 @@
 #include <stdio.h>
 #include <winsock2.h>
+#include "client.h"
+
 
 int main() {
-    // Initialize Winsock
-    WSADATA wsaData;
-
-    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
-        printf("Failed to initialize Winsock.\n");
-        return 1;
-    }
-
-    // Create a socket
-    SOCKET clientSocket = socket(AF_INET, SOCK_STREAM, 0);
-    if (clientSocket == INVALID_SOCKET) {
-        printf("Failed to create socket.\n");
-        WSACleanup();
-        return 1;
-    }
-
-    // Set up the server address
+    struct WSAData wsaData;
     struct sockaddr_in serverAddress;
-    serverAddress.sin_family = AF_INET;
-    serverAddress.sin_port = htons(1234); // Set the server port number
-    serverAddress.sin_addr.s_addr = inet_addr("127.0.0.1"); // Set the server IP address
-
-    // Connect to the server
-    if (connect(clientSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress)) < 0) {
-        printf("Failed to connect to the server.\n");
-        closesocket(clientSocket);
-        WSACleanup();
-        return 1;
-    }
-
-    // Send data to the server
-    const char* message = "Hello, server!";
-    if (send(clientSocket, message, strlen(message), 0) < 0) {
-        printf("Failed to send data to the server.\n");
-        closesocket(clientSocket);
-        WSACleanup();
-        return 1;
-    }
-
-    // Receive data from the server
+    SOCKET clientSocket;
+    char message[1024];
     char serverResponse[1024];
-    if (recv(clientSocket, serverResponse, sizeof(serverResponse), 0) < 0) {
-        printf("Failed to receive data from the server.\n");
-        closesocket(clientSocket);
-        WSACleanup();
+
+    // Configure the client socket
+    if (configure_client_socket(wsaData, serverAddress, &clientSocket) != 0) {
+        printf("Failed to configure client socket.\n");
         return 1;
     }
 
-    printf("Server response: %s\n", serverResponse);
+    // Send a message to the server
+    strcpy(message, "Hello, server!");
+    if (send_message_to_server(clientSocket, message) != 0) {
+        printf("Failed to send message to the server.\n");
+        close_socket(clientSocket);
+        return 1;
+    }
 
-    // Close the socket and cleanup Winsock
-    closesocket(clientSocket);
-    WSACleanup();
+    // Receive the server's response
+    if (get_server_response(clientSocket, serverResponse) != 0) {
+        printf("Failed to receive server response.\n");
+        close_socket(clientSocket);
+        return 1;
+    }
+
+    // Close the socket and cleanup
+    if (close_socket(clientSocket) != 0) {
+        printf("Failed to close socket.\n");
+        return 1;
+    }
 
     return 0;
 }
