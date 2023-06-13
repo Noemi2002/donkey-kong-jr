401a345f1bbf956c8469f1bad27b5ae652292fc9
Vivallo04 <andresvivallo4@gmail.com>
2023-06-12 22:55:11 -0600

    Add SDL2 library

--- src/main.c
+++ src/main.c
@@ -1,41 +1,32 @@
 #include <stdio.h>
-#include <winsock2.h>
-#include "client.h"
 
+#define SDL_MAIN_HANDLED
+#include <SDL2/SDL.h>
 
-int main() {
-    struct WSAData wsaData;
-    struct sockaddr_in serverAddress;
-    SOCKET clientSocket;
-    char message[1024];
-    char serverResponse[1024];
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
 
-    // Configure the client socket
-    if (configure_client_socket(wsaData, serverAddress, &clientSocket) != 0) {
-        printf("Failed to configure client socket.\n");
-        return 1;
+int main(int argc, char* args[]) {
+    SDL_Window* window = NULL;
+    SDL_Surface* screenSurface = NULL;
+
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+    } else {
+        window = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+
+        if (window == NULL) {
+            printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+        } else {
+            screenSurface = SDL_GetWindowSurface(window);
+            SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
+            SDL_UpdateWindowSurface(window);
+            SDL_Delay(2000);
+        }
     }
 
-    // Send a message to the server
-    strcpy(message, "Hello, server!");
-    if (send_message_to_server(clientSocket, message) != 0) {
-        printf("Failed to send message to the server.\n");
-        close_socket(clientSocket);
-        return 1;
-    }
-
-    // Receive the server's response
-    if (get_server_response(clientSocket, serverResponse) != 0) {
-        printf("Failed to receive server response.\n");
-        close_socket(clientSocket);
-        return 1;
-    }
-
-    // Close the socket and cleanup
-    if (close_socket(clientSocket) != 0) {
-        printf("Failed to close socket.\n");
-        return 1;
-    }
+    SDL_DestroyWindow(window);
+    SDL_Quit();
 
     return 0;
 }
