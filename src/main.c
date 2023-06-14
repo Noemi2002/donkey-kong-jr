4b9b8aab2e1498bab5c55e4dd65dd4bf4a8804f1
Vivallo04 <andresvivallo4@gmail.com>
2023-06-13 22:03:24 -0600

    Add base SDL window

--- src/main.c
+++ src/main.c
@@ -2,29 +2,86 @@
 
 #define SDL_MAIN_HANDLED
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_ttf.h>
+
 
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 
 int main(int argc, char* args[]) {
-    SDL_Window* window = NULL;
-    SDL_Surface* screenSurface = NULL;
-
+    // Initialize SDL
     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
         printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
-    } else {
-        window = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+        return 1;
+    }
 
-        if (window == NULL) {
-            printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
-        } else {
-            screenSurface = SDL_GetWindowSurface(window);
-            SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
-            SDL_UpdateWindowSurface(window);
-            SDL_Delay(2000);
+    // Create a window
+    SDL_Window* window = SDL_CreateWindow("SDL Hello World", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 800, 600, SDL_WINDOW_SHOWN);
+    if (window == NULL) {
+        printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    // Create a renderer
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if (renderer == NULL) {
+        printf("Renderer could not be created! SDL_Error: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    // Initialize SDL_ttf for text rendering
+    if (TTF_Init() < 0) {
+        printf("SDL_ttf could not initialize! SDL_ttf Error: %s\n", TTF_GetError());
+        return 1;
+    }
+
+    // Load a font
+    TTF_Font* font = TTF_OpenFont("D:\\Development\\Game Development\\KongJr\\assets\\fonts\\Minecraft.ttf", 2000);
+    if (font == NULL) {
+        printf("Failed to load font! SDL_ttf Error: %s\n", TTF_GetError());
+        return 1;
+    }
+
+    // Set up the text surface
+    SDL_Color textColor = { 255, 255, 255 };
+    SDL_Surface* textSurface = TTF_RenderText_Solid(font, "Hello, World!", textColor);
+    if (textSurface == NULL) {
+        printf("Failed to render text surface! SDL_ttf Error: %s\n", TTF_GetError());
+        return 1;
+    }
+
+    // Create a texture from the text surface
+    SDL_Texture* textTexture = SDL_CreateTextureFromSurface(renderer, textSurface);
+    if (textTexture == NULL) {
+        printf("Failed to create texture from surface! SDL_Error: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    // Clear the renderer
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+    SDL_RenderClear(renderer);
+
+    // Render the text texture
+    SDL_RenderCopy(renderer, textTexture, NULL, NULL);
+
+    // Update the window
+    SDL_RenderPresent(renderer);
+
+    // Event loop
+    SDL_Event event;
+    int quit = 0;
+    while (!quit) {
+        while (SDL_PollEvent(&event) != 0) {
+            if (event.type == SDL_QUIT) {
+                quit = 1;
+            }
         }
     }
 
+    // Clean up resources
+    SDL_DestroyTexture(textTexture);
+    SDL_FreeSurface(textSurface);
+    SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
 
