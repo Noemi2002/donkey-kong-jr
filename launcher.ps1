aa53a41fe9ea87c8c680fc31006ba691e7e1d609
Vivallo04 <andresvivallo4@gmail.com>
2023-06-18 00:19:16 -0600

    Fix ps1 script paths bug

--- launcher.ps1
+++ launcher.ps1
@@ -3,29 +3,42 @@ Add-Type -assembly System.Windows.Forms
 $main_form = New-Object System.Windows.Forms.Form
 $main_form.Text ='KongJr Launcher'
 
-$main_form.Width = 600
-$main_form.Height = 400
-
+$main_form.Width = 400
+$main_form.Height = 300
+$main_form.FormBorderStyle = [System.Windows.Forms.FormBorderStyle]::FixedSingle
+$main_form.MaximizeBox = $false
 
 $Label = New-Object System.Windows.Forms.Label
 $Label.Text = "KongJr Launcher"
-$Label.Location  = New-Object System.Drawing.Point(0,10)
+$Label.Location  = New-Object System.Drawing.Point(145, [int]($main_form.Height / 2 - 60))
 $Label.AutoSize = $true
 $main_form.Controls.Add($Label)
 
-# Run the java executable from 
 
-
-
-# Run the command  .\cmake-build-debug\Client.exe
 $Button = New-Object System.Windows.Forms.Button
-$Button.Text = "Run"
+$Button.Text = "Start Game"
 
-$Button.Location = New-Object System.Drawing.Point(0, 50)
+$Button.Location = New-Object System.Drawing.Point(150, [int]($main_form.Height / 2))
 
-$Button.Add_Click({& .\cmake-build-debug\Client.exe})
+function Get-ScriptExecutionPath {
+    $scriptPath = $PSScriptRoot
+    return $scriptPath
+}
 
+# Example usage
+$currentPath = Get-ScriptExecutionPath
 
+$Button.Add_Click({
+    # Specify the paths to the files
+    $serverJarPath = $currentPath.ToString() + "/server/out/artifacts/KongJrServer/KongJrServer.jar"
+    $clientExePath = $currentPath.ToString() + "/cmake-build-debug/Client.exe"
+
+    Write-Host "Concatenated path: $clientExePath"
+
+    # Execute the commands
+    Start-Process -FilePath "java" -ArgumentList "-jar $serverJarPath"
+    Start-Process -FilePath $clientExePath
+})
 
 # Add UI Element to the root form
 $main_form.Controls.Add($Button)
