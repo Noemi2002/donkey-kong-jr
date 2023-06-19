6026b73b87a1fc29cc5a9388f07e1645c8fb0002
Vivallo04 <andresvivallo4@gmail.com>
2023-06-18 23:58:19 -0600

    Add game constants

---
+++ include/globals.h
@@ -0,0 +1,17 @@
+#ifndef CLIENT_GLOBALS_H
+#define CLIENT_GLOBALS_H
+
+#endif //CLIENT_GLOBALS_H
+
+
+#define MAX_TILES 100
+
+#define TILE_SIZE 32
+
+#define SCREEN_WIDTH 640
+#define SCREEN_HEIGHT 480
+
+#define MAP_WIDTH 20
+#define MAP_HEIGHT 20
+
+#define FPS 60
