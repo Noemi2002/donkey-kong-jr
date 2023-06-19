152838b0d312441adb5395af580173c5bd38e335
Vivallo04 <andresvivallo4@gmail.com>
2023-06-19 09:52:04 -0600

    Change client connection to SDL_net client

--- src/client.c
+++ src/client.c
@@ -1,64 +1,27 @@
-#include <stdio.h>
-#include <winsock2.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_net.h>
 
-#pragma comment(lib, "ws2_32.lib")
+IPaddress ip;
+TCPsocket clientSocket;
 
-int configure_client_socket(struct WSAData data, struct sockaddr_in server_address, SOCKET* client_socket) {
-    if (WSAStartup(MAKEWORD(2, 2), &data) != 0) {
-        printf("Failed to initialize Winsock.\n");
+int initialize_client_connection(){
+    // Resolve the server IP address and port
+    if (SDLNet_ResolveHost(&ip, "127.0.0.1", 1234) == -1) {
+        printf("SDLNet_ResolveHost: %s\n", SDLNet_GetError());
+        SDLNet_Quit();
+        SDL_Quit();
         return 1;
     }
-
-    *client_socket = socket(AF_INET, SOCK_STREAM, 0);
-    if (*client_socket == INVALID_SOCKET) {
-        printf("Failed to create socket.\n");
-        WSACleanup();
-        return 1;
-    }
-
-    server_address.sin_family = AF_INET;
-    server_address.sin_port = htons(1234); // Set the server port number
-    server_address.sin_addr.s_addr = inet_addr("127.0.0.1"); // Set the server IP address
-
-    printf("Successfully initialized client.\n");
-
-    // Connect to the server
-    if (connect(*client_socket, (struct sockaddr*)&server_address, sizeof(server_address)) == SOCKET_ERROR) {
-        printf("Failed to connect to the server.\n");
-        closesocket(*client_socket);
-        WSACleanup();
-        return 1;
-    }
-
-    return 0;
 }
 
-int send_message_to_server(SOCKET client_socket, const char* message) {
-    int result = send(client_socket, message, strlen(message), 0);
-    if (result == SOCKET_ERROR) {
-        printf("Failed to send data to the server.\n");
-        return 1;
+TCPsocket open_client_socket() {
+    // Open a connection to the server
+    clientSocket = SDLNet_TCP_Open(&ip);
+    if (!clientSocket) {
+        printf("SDLNet_TCP_Open: %s\n", SDLNet_GetError());
+        SDLNet_Quit();
+        SDL_Quit();
     }
 
-    printf("Successfully sent message to server.\n");
-    return 0;
-}
-
-int get_server_response(SOCKET client_socket, char server_response[1024]) {
-    int result = recv(client_socket, server_response, 1024, 0);
-    if (result == SOCKET_ERROR) {
-        printf("Failed to receive data from the server.\n");
-        return 1;
-    }
-
-    server_response[result] = '\0'; // Null-terminate the received data
-    printf("Server response: %s\n", server_response);
-    return 0;
-}
-
-int close_socket(SOCKET client_socket) {
-    printf("Closing socket.\n");
-    closesocket(client_socket);
-    WSACleanup();
-    return 0;
+    return clientSocket;
 }
